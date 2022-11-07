07967bf17aa44e2d53e6062df26b1e67b74cdeec
zy <1661996964@qq.com>
2022-11-07 21:43:10 +0800

    @Scope("prototype")  //设置单例或多例 ，与XML中bean scope 完全相同； @PostConstruct   //XML 中init-method完全相同

--- src/main/java/com/zy/spring/ioc/service/UserService.java
+++ src/main/java/com/zy/spring/ioc/service/UserService.java
@@ -3,14 +3,22 @@ package com.zy.spring.ioc.service;
 import com.zy.spring.ioc.dao.IUserDao;
 import com.zy.spring.ioc.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
+@Scope("prototype")  //设置单例或多例 ，与XML中bean scope 完全相同
 public class UserService {
     @Autowired
     // spring IoC容器通过反射技术将属性private修饰符自动转为public，自动进行赋值，不再执行setter方法
     private UserDao userDao;
 
+    @PostConstruct   //XML 中init-method完全相同
+    public void init(){
+        System.out.println("初始化UserService对象");
+    }
     private IUserDao udao;
 
     public IUserDao getUdao(){
